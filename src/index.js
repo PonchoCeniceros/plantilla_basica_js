b2eb9cd668cc38acd729f2809b53becef88c6da9
Giovanny Alfonso Chávez Ceniceros <gio.icc.uach@gmail.com>
2024-08-12 20:29:56 -0600

    [main] adding hello world message

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ import dotenv from 'dotenv';
  * configuración de las variables de entorno
  */
 dotenv.config();
+
+(() => {
+  /**
+   * Hola mundo!!
+   */
+  console.log('Hello world!');
+
+})();
